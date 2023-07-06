883dec0746811ea56a218731ee5180fa2d33e437
João Paulo <joaowais@gmail.com>
2023-07-06 15:16:34 -0300

    criada a classe Personagem

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
+// const personagemPedrinho = {
+//     nome: 'Pedrinho',
+//     vida: 7,
+//     mana: 12,
+//     level: 5,
+//     tipo: 'Mago',
+// }
+
+// const personagemJose = {
+//     nome: 'Jose',
+//     vida: 7,
+//     mana: 6,
+//     level: 3,
+//     tipo: 'Arqueiro',
+// }
+
+
+class Personagem {
+    nome;
+    vida;
+    mana;
+    level;
+    tipo;
+
 }
 
-const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}
+const personagemPedrinho = new Personagem();
+personagemPedrinho.nome = 'Pedrinho';
+personagemPedrinho.vida = 7;
+personagemPedrinho.mana = 12;
+personagemPedrinho.level = 5;
+personagemPedrinho.tipo = 'Mago';
 
+console.log(personagemPedrinho)
