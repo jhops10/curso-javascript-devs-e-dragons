def57c682572c4e9e907ddb2a684277134968ccc
João Paulo <joaowais@gmail.com>
2023-07-06 23:42:12 -0300

    adicionado constructor ao personagem-view.js e renderizando as cartas com .render()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { Personagem } from "./modules/Personagem.js";
+import { PersonagemView } from "./components/personagem-view.js";
 
 const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
 const personagemJose = new Personagem('Jose', 3, 'Arqueiro');
 
-console.log(personagemJose);
-console.log(personagemPedrinho);
+const personagens = [personagemPedrinho, personagemJose];
+
+new PersonagemView(personagens).render();
