baaad6fed83ddae92cc7f81c0e6ed64c9c3a8941
João Paulo <joaowais@gmail.com>
2023-07-07 17:54:28 -0300

    adicionado a classe arqueiro-mago

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import { Personagem } from "./modules/Personagem.js";
 import { PersonagemView } from "./components/personagem-view.js";
 import { Mago } from "./modules/Mago.js";
 import { Arqueiro } from "./modules/Arqueiro.js";
+import { ArqueiroMago } from "./modules/Arqueiro-Mago.js";
 
 const magoJhops = new Mago('Jhops', 10, 'Fogo', 6, 10);
 const magoMerlin = new Mago('Merlin', 5, 'Gelo', 3, 5);
-const paladoxJhops = new Arqueiro('Paladox', 5, 10);
 const robinHood = new Arqueiro('Robin Hood', 1, 3);
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'Vento', 4, 8);
 
-const personagens = [magoJhops, magoMerlin, paladoxJhops, robinHood];
+const personagens = [magoJhops, magoMerlin, paladoxJhops, robinHood, arqueiroMagoChico];
 
 new PersonagemView(personagens).render();
 
