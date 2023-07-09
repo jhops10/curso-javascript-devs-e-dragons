51170b9a3ae9e1e0686ae76210b650be5c2f1c9e
João Paulo <joaowais@gmail.com>
2023-07-09 10:20:44 -0300

    adicionado funcoes aos botoes de aumentar e diminuir level

--- src/modules/Personagem.js
+++ src/modules/Personagem.js
@@ -11,8 +11,22 @@ export class Personagem {
 
   }
 
+  aumentarLevel() {
+    this.level += 1;
+  }
+
+  diminuirLevel() {
+    this.level -= 1;
+  }
+
   get level() {
     return this.#level;
+  }
+
+  set level(novoLevel) {
+    if (novoLevel >= 1 && novoLevel <= 10) {
+      this.#level = novoLevel;
+    }
   }
 
   obterInsignia() {
